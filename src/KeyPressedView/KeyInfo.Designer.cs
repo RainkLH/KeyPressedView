fc8bc915ce097a26247961832364577a5e00ad0d
罗昊 <luoh@mctech.vip>
2022-11-18 18:18:06 +0800

    嗯

---
+++ src/KeyPressedView/KeyInfo.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace KeyPressedView
+{
+    partial class KeyInfo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeyInfo));
+            this.notifyIconM = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuItem_Quit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemSetStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemCancelStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.label = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIconM
+            // 
+            this.notifyIconM.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIconM.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconM.Icon")));
+            this.notifyIconM.Text = "喵~";
+            this.notifyIconM.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItem_Quit,
+            this.menuItemSetStart,
+            this.menuItemCancelStart});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 70);
+            // 
+            // menuItem_Quit
+            // 
+            this.menuItem_Quit.Name = "menuItem_Quit";
+            this.menuItem_Quit.Size = new System.Drawing.Size(148, 22);
+            this.menuItem_Quit.Text = "退出";
+            this.menuItem_Quit.Click += new System.EventHandler(this.menuItem_Quit_Click);
+            // 
+            // menuItemSetStart
+            // 
+            this.menuItemSetStart.Name = "menuItemSetStart";
+            this.menuItemSetStart.Size = new System.Drawing.Size(148, 22);
+            this.menuItemSetStart.Text = "打开开机自启";
+            this.menuItemSetStart.Click += new System.EventHandler(this.menuItemSetStart_Click);
+            // 
+            // menuItemCancelStart
+            // 
+            this.menuItemCancelStart.Name = "menuItemCancelStart";
+            this.menuItemCancelStart.Size = new System.Drawing.Size(148, 22);
+            this.menuItemCancelStart.Text = "取消开机自启";
+            this.menuItemCancelStart.Click += new System.EventHandler(this.menuItemCancelStart_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(0, 0);
+            this.label.Margin = new System.Windows.Forms.Padding(0);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(91, 46);
+            this.label.TabIndex = 0;
+            this.label.Text = "Text";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // KeyInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 60);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "KeyInfo";
+            this.Opacity = 0.5D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconM;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem_Quit;
+        private System.Windows.Forms.ToolStripMenuItem menuItemSetStart;
+        private System.Windows.Forms.ToolStripMenuItem menuItemCancelStart;
+    }
+}
---
+++ src/KeyPressedView/KeyInfo.cs
@@ -0,0 +1,148 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace KeyPressedView
+{
+    public partial class KeyInfo : Form
+    {
+        private const string SoftWareName = "Raink.KeyPressedView";
+        private IKeyboardMouseEvents m_GlobalHook;
+        private int _screenWidth;
+        private int _screenHeight;
+        private DateTime _timeShow;
+        private bool _isRuning;
+        public KeyInfo()
+        {
+            InitializeComponent();
+            _isRuning = true;
+            this.ShowInTaskbar = false;
+            _screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            _screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            this.Location = new Point(_screenWidth - 270, _screenHeight - 70);
+            this.Visible = false;
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyDown += M_GlobalHook_KeyDown;
+            ShowThread();
+        }
+
+        private void M_GlobalHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.label.Text = e.KeyData.ToString();
+            _timeShow = DateTime.Now;
+            this.Visible = true;
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            m_GlobalHook.KeyDown -= M_GlobalHook_KeyDown;
+            m_GlobalHook.Dispose();
+        }
+
+        private async Task ShowThread()
+        {
+            await Task.Run(() =>
+            {
+                while (_isRuning)
+                {
+                    if (this.Visible == true)
+                    {
+                        var timeSpan = DateTime.Now - _timeShow;
+                        if (timeSpan.TotalMilliseconds > 1000)
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                this.Visible = false;
+                            }));
+                        }
+                    }
+                }
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isRuning = false;
+        }
+
+        private void menuItem_Quit_Click(object sender, EventArgs e)
+        {
+            _isRuning = false;
+            this.Close();
+        }
+
+        private void SetStartWithStart()
+        {
+            string path = Application.ExecutablePath;
+            RegistryKey rk = Registry.CurrentUser; // 添加到 当前登陆用户的 注册表启动项     
+            try
+            {
+                //SetValue:存储值的名称   
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+
+                // 检测是否之前有设置自启动了，如果设置了，就看值是否一样
+                string old_path = (string)rk2.GetValue(SoftWareName);
+
+                if (!path.Equals(old_path))
+                {
+                    rk2.SetValue(SoftWareName, path);
+                    MessageBox.Show("设置开机自启成功", "KeyPressedView");
+                }
+                else
+                {
+                    MessageBox.Show("已设置过开机自启", "KeyPressedView");
+                }
+
+                rk2.Close();
+                rk.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"开机自启动设置失败:{e}", "KeyPressedView");
+            }
+        }
+
+        private void CancelStartWithStart()
+        {
+            string path = Application.ExecutablePath;
+            RegistryKey rk = Registry.CurrentUser;
+            try
+            {
+                // SetValue: 存储值的名称
+                RegistryKey rk2 = rk.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+
+                string old_path = (string)rk2.GetValue(SoftWareName);
+
+                rk2.DeleteValue(SoftWareName, false);
+                MessageBox.Show("已取消开启自启", "KeyPressedView");
+                rk2.Close();
+                rk.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"未能正常取消开机自启:{e}", "KeyPressedView");
+            }
+        }
+
+        private void menuItemSetStart_Click(object sender, EventArgs e)
+        {
+            SetStartWithStart();
+        }
+
+        private void menuItemCancelStart_Click(object sender, EventArgs e)
+        {
+            CancelStartWithStart();
+        }
+    }
+}
